db245c139b68b55b3acb79e00c19b0d7ce2641fe
just Mercy <125640680+NTMERCY@users.noreply.github.com>
2023-10-16 14:23:09 +0700

    update

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from bson import ObjectId
 
 app = Flask(__name__)
 
+client=MongoClient('mongodb+srv://test:sparta@cluster0.qpc2myf.mongodb.net/?retryWrites=true&w=majority')
+db=client.profil
 
 @app.route('/')
 def home():
@@ -27,7 +29,7 @@ def product():
 def gallery():
     return render_template('gallery.html')
 
-@app.route('/contactus')
+@app.route('/contactus',methods=['GET'])
 def contactus():
     return render_template('contact.html')
 
@@ -39,8 +41,8 @@ def login():
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/edit')
-def edit():
+@app.route('/editdata')
+def editdata():
     return render_template('editdata.html')
 
 @app.route('/tabel')
@@ -55,8 +57,23 @@ def pesan():
 def tambahdata():
     return render_template('tambahdata.html')
 
+@app.route('/contactus',methods=['POST'])
+def tambah():
+    name=request.form.get('name_give')
+    email=request.form.get('email_give')
+    message=request.form.get('message_give')
+    print(name,email,message)
 
+    doc={
+        'name':name,
+        'email':email,
+        'message':message,
+    }
 
+    db.buah.insert_one(doc)
+    return jsonify({
+        'msg':'berhasil terkirim!'
+    })
 
 if __name__=='__main__':
     app.run('0.0.0.0',port=5000, debug=True)
