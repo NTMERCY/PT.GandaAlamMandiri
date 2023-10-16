f5a182a3791a17a0b20c8b19ebb58d1f7c47615d
just Mercy <125640680+NTMERCY@users.noreply.github.com>
2023-10-16 22:59:14 +0700

    update tambah gambar dan edit

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def product():
 
 @app.route('/gallery')
 def gallery():
-    return render_template('gallery.html')
+    data = list(db.pict.find({}).sort('_id',-1))
+    return render_template('gallery.html',data=data)
 
 @app.route('/contactus',methods=['GET'])
 def contactus():
@@ -39,15 +40,39 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    data = list(db.pict.find({}).sort('_id',-1))
+    return render_template('dashboard.html',data=data)
 
 @app.route('/editdata')
 def editdata():
-    return render_template('editdata.html')
+    id =request.args.get('_id')
+    data=list(db.pict.find({'_id' : ObjectId(id)}))
+    return render_template('editdata.html', data=data)
 
-@app.route('/tabel')
+@app.route('/editdata', methods=['POST'])
+def editPost():
+    id = request.form.get('id_give')
+    file = request.files.get('file_give')
+    extension = file.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'file-{mytime}.{extension}'
+    save_to = f'static/{filename}'
+    file.save(save_to)
+
+
+    doc={
+        'file': filename,
+    }
+
+    db.pict.update_one({'_id':ObjectId(id)}, {'$set':doc})
+    return jsonify({ 'msg':'foto berhasil diubah' })
+
+
+@app.route('/tabel',methods=['GET'])
 def tabel():
-    return render_template('tabel.html')
+    data = list(db.pict.find({}).sort('_id',-1))
+    return render_template('tabel.html',data=data)
 
 @app.route('/pesan')
 def pesan():
@@ -58,6 +83,24 @@ def pesan():
 @app.route('/tambahdata')
 def tambahdata():
     return render_template('tambahdata.html')
+
+@app.route('/tambahdataPost', methods=['POST'])
+def tambahdataPost():
+    file = request.files.get('file_give')
+    extension = file.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'file-{mytime}.{extension}'
+    save_to = f'static/{filename}'
+    file.save(save_to)
+
+
+    doc={
+        'file': filename,
+    }
+
+    db.pict.insert_one(doc)
+    return jsonify({ 'msg':'berhasil terkirim!' })
 
 @app.route('/contactusPost', methods=['POST'])
 def tambah():
@@ -74,6 +117,14 @@ def tambah():
 
     db.msg.insert_one(doc)
     return jsonify({ 'msg':'berhasil terkirim!' })
+
+@app.route('/deletePhoto', methods=['POST'])
+def delete_photo():
+    #mengambil id dari sisi client
+    id = request.form.get('id_give')
+    #menghapus foto berdasarkan foto yang dippilih dengan menggunakan id pada foto tersebut
+    db.pict.delete_one({'_id':ObjectId(id)})
+    return jsonify({'msg':'Foto berhasil dihaopus!'})
 
 if __name__=='__main__':
     app.run('0.0.0.0',port=5000, debug=True)
